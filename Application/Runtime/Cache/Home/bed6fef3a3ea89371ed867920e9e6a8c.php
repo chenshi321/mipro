c4f074fb74a2e6e9e7e9f21a6cd79b2f23cd211e
chenshi321 <2571633739@qq.com>
2017-11-16 21:18:57 +0800

    first commit

---
+++ Application/Runtime/Cache/Home/bed6fef3a3ea89371ed867920e9e6a8c.php
@@ -0,0 +1,150 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title><?php echo ($page_title); ?></title>
+	<meta name="keywords" content="<?php echo ($page_keywords); ?>" />
+	<meta name="description" content="<?php echo ($page_description); ?>" />
+	<link rel="stylesheet" href="/Public/Home/style/base.css" type="text/css">
+	<link rel="stylesheet" href="/Public/Home/style/global.css" type="text/css">
+	<link rel="stylesheet" href="/Public/Home/style/header.css" type="text/css">
+	<?php foreach ($page_css as $k => $v): ?>
+	<link rel="stylesheet" href="/Public/Home/style/<?php echo ($v); ?>.css" type="text/css">
+	<?php endforeach; ?>
+	<link rel="stylesheet" href="/Public/Home/style/bottomnav.css" type="text/css">
+	<link rel="stylesheet" href="/Public/Home/style/footer.css" type="text/css">
+	<script type="text/javascript" src="/Public/Home/js/jquery-1.8.3.min.js"></script>
+	<script type="text/javascript" src="/Public/Home/js/header.js"></script>
+	<?php foreach ($page_js as $k => $v): ?>
+	<script type="text/javascript" src="/Public/Home/js/<?php echo ($v); ?>.js"></script>
+	<?php endforeach; ?>
+</head>
+<body>
+	<!-- 顶部导航 start -->
+	<div class="topnav">
+		<div class="topnav_bd w1210 bc">
+			<div class="topnav_left">
+				
+			</div>
+			<div class="topnav_right fr">
+				<ul>
+					<li id="logInfo">您好，欢迎来到京西！[<a href='<?php echo U('Home/Member/login'); ?>'>登录</a>] [<a href='<?php echo U('Home/Member/regist'); ?>'>免费注册</a>]</li>
+					<li class="line">|</li>
+					<li>我的订单</li>
+					<li class="line">|</li>
+					<li>客户服务</li>
+
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- 顶部导航 end -->
+	<div style="clear:both;"></div>
+
+	<!-- 内容 -->
+	
+<!-- 登录主体部分start -->
+	<div class="login w990 bc mt10 regist">
+		<div class="login_hd">
+			<h2>用户注册</h2>
+			<b></b>
+		</div>
+		<div class="login_bd">
+			<div class="login_form fl">
+				<form action="" method="post">
+					<ul>
+						<li>
+							<label for="">Email：</label>
+							<input type="text" class="txt" name="email" />
+							<p>请输入正确的Email地址</p>
+						</li>
+						<li>
+							<label for="">密码：</label>
+							<input type="password" class="txt" name="password" />
+							<p>6-20位字符，可使用字母、数字和符号的组合，不建议使用纯数字、纯字母、纯符号</p>
+						</li>
+						<li>
+							<label for="">确认密码：</label>
+							<input type="password" class="txt" name="cpassword" />
+							<p> <span>请再次输入密码</p>
+						</li>
+						<li>
+							<label for="">验证码：</label>
+							<input class="txt" type="text"  name="chkcode" /><br />
+						</li>
+						<li>
+							<label for="">&nbsp;</label>
+							<img onclick="this.src='<?php echo U('chkcode'); ?>#'+Math.random();" style="cursor:pointer;" src="<?php echo U('chkcode'); ?>" alt="" />
+							<span>看不清？<a onclick="$(this).parent().prev('img').trigger('click');" href="javascript:void(0);">换一张</a></span>
+						</li>
+						<li>
+							<label for="">&nbsp;</label>
+							<input name="mustclick" type="checkbox" class="chb" checked="checked" /> 我已阅读并同意《用户注册协议》
+						</li>
+						<li>
+							<label for="">&nbsp;</label>
+							<input type="submit" value="" class="login_btn" />
+						</li>
+					</ul>
+				</form>
+
+				
+			</div>
+			
+			<div class="mobile fl">
+				<h3>手机快速注册</h3>			
+				<p>中国大陆手机用户，编辑短信 “<strong>XX</strong>”发送到：</p>
+				<p><strong>1069099988</strong></p>
+			</div>
+
+		</div>
+	</div>
+	<!-- 登录主体部分end -->
+	
+	<!-- 底部导航 end -->
+	<div style="clear:both;"></div>
+	<!-- 底部版权 start -->
+	<div class="footer w1210 bc mt10">
+		<p class="links">
+			<a href="">关于我们</a> |
+			<a href="">联系我们</a> |
+			<a href="">人才招聘</a> |
+			<a href="">商家入驻</a> |
+			<a href="">千寻网</a> |
+			<a href="">奢侈品网</a> |
+			<a href="">广告服务</a> |
+			<a href="">移动终端</a> |
+			<a href="">友情链接</a> |
+			<a href="">销售联盟</a> |
+			<a href="">京西论坛</a>
+		</p>
+		<p class="copyright">
+			 © 2005-2013 京东网上商城 版权所有，并保留所有权利。  ICP备案证书号:京ICP证070359号 
+		</p>
+		<p class="auth">
+			<a href=""><img src="/Public/Home/images/xin.png" alt="" /></a>
+			<a href=""><img src="/Public/Home/images/kexin.jpg" alt="" /></a>
+			<a href=""><img src="/Public/Home/images/police.jpg" alt="" /></a>
+			<a href=""><img src="/Public/Home/images/beian.gif" alt="" /></a>
+		</p>
+	</div>
+	<!-- 底部版权 end -->
+
+</body>
+</html>
+<script>
+// AJAX判断登录状态
+$.ajax({
+	type : "GET",
+	url : "<?php echo U('Home/Member/ajaxChkLogin'); ?>",
+	dataType : "json",  // 指定服务器返回json
+	success : function(data)
+	{
+		if(data.ok == 1)
+			var html = "您好，"+data.email+" <a href='<?php echo U('Home/Member/logout'); ?>'>[退出]</a>";
+		else
+			var html = "您好，欢迎来到京西！[<a href='<?php echo U('Home/Member/login'); ?>'>登录</a>] [<a href='<?php echo U('Home/Member/regist'); ?>'>免费注册</a>] ";
+		$("#logInfo").html(html);
+	}
+});
+</script>
